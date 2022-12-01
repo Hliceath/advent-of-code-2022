1695c60df543a82f146ca37639f721a1856531b3
Hliceath <hliceath@protonmail.com>
2022-12-02 00:17:31 +0100

    style: fix text printed

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import { day1 } from "./day1/day1";
 
-console.log('🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄');
-console.log('🎄Advent of Code 2022🎄');
-console.log('🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄');
+console.log('🎄 Advent of Code 2022 🎄 \n');
 
 day1().catch(console.error);
