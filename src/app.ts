ece5adcec846da0fbd953e5421b6ab6a2eaa425f
Hliceath <hliceath@protonmail.com>
2022-12-09 10:40:44 +0100

    feat: add day 5 solutions

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { day1 } from "./day1/day1";
 import { day2 } from "./day2/day2";
 import { day3 } from "./day3/day3";
 import { day4 } from "./day4/day4";
+import { day5 } from "./day5/day5";
 
 console.log('🎄 Advent of Code 2022 🎄 \n');
 
@@ -9,3 +10,4 @@ day1().catch(console.error);
 day2().catch(console.error);
 day3().catch(console.error);
 day4().catch(console.error);
+day5().catch(console.error);
---
+++ src/day5/day5.ts
@@ -0,0 +1,64 @@
+import { readFile } from "../utils/utils";
+import { stacks } from '../models/day5.models';
+
+export async function day5(): Promise<void> {
+    const rl = readFile('src/day5/input.txt');
+
+    const part1Stacks: stacks = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: []
+    };
+
+    const part2Stacks: stacks = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: []
+    };
+
+    let i = 0;
+    for await (const line of rl) {
+        if (i < 8) {
+            line.split('').forEach((letter, index) => {
+                if (index % 4 === 1) {
+                    if (letter !== " ") part1Stacks[(index - 1) / 4 + 1].unshift(letter);
+                    if (letter !== " ") part2Stacks[(index - 1) / 4 + 1].unshift(letter);
+                }
+            });
+        }
+
+        if (line.startsWith("move")) {
+            const lineSplitted = line.split(' ');
+            const toMoveP2 = [];
+            for (let i = 0; i < Number(lineSplitted[1]); i++) {
+                const toMoveP1 = part1Stacks[Number(lineSplitted[3])].pop();
+                if (toMoveP1) part1Stacks[Number(lineSplitted[5])].push(toMoveP1);
+
+                toMoveP2.push(part2Stacks[Number(lineSplitted[3])].pop());
+            }
+            toMoveP2.reverse();
+            toMoveP2.forEach((letter) => { if (letter) part2Stacks[Number(lineSplitted[5])].push(letter) });
+        }
+        i++;
+    }
+    let part1Answer=  "";
+    Object.keys(part1Stacks).forEach((stack) => part1Answer += part1Stacks[Number(stack)].pop());
+    let part2Answer=  "";
+    Object.keys(part2Stacks).forEach((stack) => part2Answer += part2Stacks[Number(stack)].pop());
+
+    console.log(`Day 5:
+    Part 1) CrateMover 9000 stacks are: ${part1Answer}. 
+    Part 2) CrateMover 9001 stacks are: ${part2Answer} \n`);
+}
